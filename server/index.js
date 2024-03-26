41ecae3369eb131f653b32171406fe2a8e027de2
Gourab Das <144624498+GourabDas2000@users.noreply.github.com>
2024-03-27 01:22:03 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -9,7 +9,10 @@ dotenv.config();
 const MAIN_URL = process.env.MAIN_URL;
 const PORT = process.env.PORT;
 
-
+app.get('/', (req, res) => {
+    res.send('This is it')
+});
+app.set(cors());
 // app.use(cors({
 //     origin: ["http://localhost:5173/"],
 //     methods: ["POST", "GET", "PUT", "DELETE"],
