c3cc93d936fdae5919caac5e28ba3499d7b5f665
gourab Das <bisal.das2000@gmail.com>
2024-03-27 18:12:08 +0530

    ok

--- server/index.js
+++ server/index.js
@@ -11,7 +11,7 @@ const PORT = process.env.PORT;
 
 
 app.use(cors({
-    origin: ["http://localhost:5173/"],
+    origin: "*",
     methods: ["POST", "GET", "PUT", "DELETE"],
     credentials: true
 }));
