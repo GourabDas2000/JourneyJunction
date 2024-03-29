7317a957dbd621ffefe27f476149fda9613ab063
gourab Das <bisal.das2000@gmail.com>
2024-03-29 15:48:14 +0530

    newres added

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -8,7 +8,7 @@ function App() {
     try{
       const result = await fetch(`${BASE_URL}alltext`)
       const res = await result.json()
-      console.log(res)
+      console.log('res=',res)
     }catch(e){
       console.log(e)
     }
@@ -17,7 +17,6 @@ function App() {
     <>
      <input type='text' onChange={(e) => settext(e.target.value)}/>
      <button onClick={handlesubmit}>Click to store the text</button>
-     <h1>Ready to go</h1>
     </>
   )
 }
