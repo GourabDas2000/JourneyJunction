053b61ce1acfc03e1572b25dc9e2e7b294e591e0
Gourab Das <gourabdas10122000@gmail.com>
2024-03-29 14:06:52 +0530

    Testing commits

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -17,7 +17,6 @@ function App() {
     <>
      <input type='text' onChange={(e) => settext(e.target.value)}/>
      <button onClick={handlesubmit}>Click to store the text</button>
-     <h1>Ready to go</h1>
     </>
   )
 }
